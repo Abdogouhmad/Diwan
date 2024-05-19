056a21c7f64241cd69ed8ec424df42ca4229db13
Abdogouhmad <gouhmada@gmail.com>
2024-05-19 20:31:32 +0100

    the cli is ready to execute

--- diwan-cli/src/main.rs
+++ diwan-cli/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+// use anyhow::Context;
+// use std::fs::File;
+// use std::io::BufReader;
+use clap::Parser;
+use std::path::PathBuf;
+
+/// diwan is a simple Rust text editor
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to the file
+    #[arg(value_name = "FILE")]
+    file: Option<PathBuf>,
+}
+
+fn main() -> anyhow::Result<()> {
+    // Parse the arguments
+    let args = Args::parse();
+
+    // If a file path is provided, open the editor with that file
+    if let Some(path) = args.file {
+        // Open the file
+        // let file = File::open(&path).context("failed to open file")?;
+        // let _ = BufReader::new(file);
+
+        // TODO: open editor with the file
+        println!("{}", path.display());
+    } else {
+        // TODO: run editor without any file
+        println!("run editor");
+    }
+
+    // init editor
+    //let mut editor = diwan::Editor::new();
+
+    // run editor
+    //editor.run();
+
+    Ok(())
 }
