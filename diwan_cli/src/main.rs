b04f9a175730b802242460bde321137cd32da397
Abdogouhmad <gouhmada@gmail.com>
2024-05-22 00:59:19 +0100

    clap is removed to reduce the unused crate

--- diwan_cli/src/main.rs
+++ diwan_cli/src/main.rs
@@ -1,29 +1,25 @@
 use anyhow::Context;
-use clap::Parser;
 use diwan_core::editor::Editor;
+use std::env;
 use std::path::PathBuf;
 
-/// diwan is a simple Rust text editor
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Path to the file
-    #[arg(value_name = "FILE")]
-    file: Option<PathBuf>,
-}
-
 fn main() -> anyhow::Result<()> {
-    // Parse the arguments
-    let args = Args::parse();
+    // Get the command-line arguments
+    let args: Vec<String> = env::args().collect();
 
-    // If a file path is provided, print the file path
-    if let Some(path) = args.file {
-        println!(" hey {}", path.display());
+    // If there is at least one argument after the program name
+    if args.len() > 1 {
+        // Assume the first argument after the program name is the file path
+        let file_path = PathBuf::from(&args[1]);
+        println!("Opening file: {}", file_path.display());
+        // Handle the file path here
     } else {
         // Create a new editor instance
-        let mut editor = Editor::new().context("Failed to return instance of editor")?;
+        let mut editor = Editor::new().context("Failed to create instance of editor")?;
+
         // Run the editor
         editor.run().context("Failed to load Diwan editor")?;
+
         // Drop the editor to restore terminal state
         drop(editor);
     }
